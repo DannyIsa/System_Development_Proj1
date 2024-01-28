f526989523e6054d991dfebf6e5ca6fec5c27841
Daniel Isakov <daniisakov@gmail.com>
2024-01-28 14:51:42 +0200

    main completed

--- main.c
+++ main.c
@@ -12,12 +12,32 @@ int main()
     printf("Enter Second Num: ");
     scanf("%d", &secNum);
 
-    // for (int currentNum = firstNum; currentNum <= secNum; currentNum++)
-    // {
-    //     if (isArmstrongRecursion(currentNum))
-    //         armstrongNums[counter] = currentNum;
+    for (int currentNum = firstNum; currentNum <= secNum; currentNum++)
+    {
+        if (isPrime(currentNum))
+            printf("%d ", currentNum);
+    }
+    printf("\n");
 
-    // }
+    for (int currentNum = firstNum; currentNum <= secNum; currentNum++)
+    {
+        if (isArmstrongRecursion(currentNum))
+            printf("%d ", currentNum);
+    }
+    printf("\n");
+
+    for (int currentNum = firstNum; currentNum <= secNum; currentNum++)
+    {
+        if (isStrong(currentNum))
+            printf("%d ", currentNum);
+    }
+    printf("\n");
+
+    for (int currentNum = firstNum; currentNum <= secNum; currentNum++)
+    {
+        if (isPalindromeRecursion(currentNum))
+            printf("%d ", currentNum);
+    }
 
     return 0;
 }
